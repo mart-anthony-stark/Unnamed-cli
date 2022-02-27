2fec36ac4ff99c8e3453a67f2621d5c6e74f4bb9
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-27 20:08:35 +0800

    add: prompt

--- index.js
+++ index.js
@@ -6,4 +6,27 @@ import { fileURLToPath } from "url";
 const CURR_DIR = process.cwd();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-console.log(__dirname);
+const CHOICES = ["mongoose-starter"];
+
+const QUESTIONS = [
+  {
+    name: "template",
+    type: "list",
+    message: "What project template would you like to generate?",
+    choices: CHOICES,
+  },
+  {
+    name: "name",
+    type: "input",
+    message: "Project name:",
+    validate: function (input) {
+      if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
+      else
+        return "Project name may only include letters, numbers, underscores and hashes.";
+    },
+  },
+];
+
+inquirer.prompt(QUESTIONS).then(({ template, name }) => {
+    
+});
