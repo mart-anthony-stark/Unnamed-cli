0a7e9e71ae3483e264b6facb7fde8b3709eaf487
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-28 22:10:46 +0800

    add loader class

--- src/loader.js
+++ src/loader.js
@@ -1,18 +1,25 @@
 import colors from "colors";
 
-class Loader{
-    
+class Loader {
+  constructor() {
+    this.loading = null;
+  }
+
+  startLoading() {
+    let P = ["\\", "|", "/", "-"];
+    let x = 0;
+    this.loading = setInterval(() => {
+      process.stdout.write(
+        "  Downloading Template...".cyan + "\r" + P[x++].red + "   "
+      );
+      x &= 3;
+    }, 50);
+  }
+
+  stopLoading() {
+    clearInterval(this.loading);
+    process.stdout.write("\r");
+  }
 }
 
-var P = ["\\", "|", "/", "-"];
-var x = 0;
-const loading = setInterval(() => {
-  process.stdout.write(
-    "  Downloading Template...".cyan + "\r" + P[x++].red + "   "
-  );
-  x &= 3;
-}, 50);
-setTimeout(() => {
-  clearInterval(loading);
-  process.stdout.write("\r");
-}, 2000);
+export default Loader;
