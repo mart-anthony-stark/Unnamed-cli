4e1182f77b96523fcd41c1957d41585789ad2eac
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-28 09:36:20 +0800

    moved

--- version.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { createRequire } from "module";
-import colors from "colors";
-const require = createRequire(import.meta.url);
-const version = require("./package.json").version;
-
-console.log("unnamed-cli version: ".red + version.cyan);
-console.log("Created by mart-anthony-stark".red);
