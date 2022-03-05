3de914afaff7bfd667be5e3dc8000f9edad75193
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-05 22:13:10 +0800

    add index

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 #!/usr/bin/env node
 
-import inquirer from "inquirer";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import templates from "./src/templates.js";
-import cloneRepo from "./src/clone-template.js";
-const CURR_DIR = process.cwd();
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { argv } from "process";
+import { createRequire } from "module";
+import colors from "colors";
+import create from "./src/create.js";
+const require = createRequire(import.meta.url);
+const version = require("./package.json").version;
 
-const choices = templates.map((temp) => temp.templateName);
-
-const QUESTIONS = [
-  {
-    name: "template",
-    type: "list",
-    message: "What project template would you like to generate?",
-    choices,
-  },
-  {
-    name: "name",
-    type: "input",
-    message: "Project name:",
-    validate: function (input) {
-      if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
-      else
-        return "Project name may only include letters, numbers, underscores and hashes.";
-    },
-  },
-];
-
-inquirer.prompt(QUESTIONS).then(({ template, name }) => {
-  cloneRepo(template, name);
-});
+const flag = argv[2];
+if (flag === "new") {
+  create();
+} else if (flag === "-v" || flag === "--version") {
+  console.log("unnamed-cli version: ".red + version.cyan);
+  console.log("Created by mart-anthony-stark".red);
+} else {
+  console.log(`Unknown flag: ${flag}`.red);
+  console.log("unnamed-cli [flag]".cyan);
+  console.log("-v             returns cli version".blue);
+  console.log("-version       returns cli version".blue);
+}
